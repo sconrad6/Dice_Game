f129516cb1485d979461a545c92e0aa964950ffe
Savanah Conrad <sibgym@yahoo.com>
2019-07-17 15:50:07 -0400

    First commit

---
+++ Dice_Rolling/DiceRolling.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Dice_Rolling
+{
+    public class DiceRolling
+    {
+    
+        private readonly int userNumber;
+
+        public DiceRolling(int userNumber)
+        {
+            this.userNumber = userNumber;
+        }
+
+        // Creates two random numbers between one and the user's number
+        public static string DiceRolls(int userNumber)
+        {
+            Random randomNumber = new Random();
+
+            int roll1 = randomNumber.Next(1, userNumber);
+            int roll2 = randomNumber.Next(1, userNumber);
+            Console.WriteLine($"\nYou rolled {roll1} and {roll2}");
+
+            if (roll1 == 1 && roll2 == 1)
+            {
+                return "That's snake eyes!";
+            }
+            else if (roll1 == 6 && roll2 == 6)
+            {
+                return "Woot woot you got some boxcars!";
+            }
+            else if (roll1 + roll2 == 7 || roll1 + roll2 == 11)
+            {
+                return "You won craps! But that sure isn't crappy!!";
+            } else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ Dice_Rolling/DiceRollingTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Dice_Rolling
+{
+    class DiceRollingTest
+    {
+        static void Main(string[] args)
+        {
+            string rollAgain = "Y";
+            Console.WriteLine("Welcome to the Dice Roll! Would you like to roll the dice? Y/N.");
+            string userAnswer = Console.ReadLine().ToUpper();
+            do
+            {
+                // Ask the user how many sides they want their dice to have
+                // Check to make sure the user has entered a valid number
+                // and if they don't, ask them to enter another one
+                Console.WriteLine("How many sides do you want your die to have?");
+                try
+                {
+                    int userNumber = int.Parse(Console.ReadLine());
+                    Console.WriteLine(DiceRolling.DiceRolls(userNumber));
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid.  Please try again.");
+                    continue;
+                }
+
+                // Ask user if they want to roll again, if they don's the 
+                //program will end, if they do it will continue
+                Console.WriteLine("\nWould you like to roll again? Y/N");
+                rollAgain = Console.ReadLine().ToUpper();
+
+            } while (rollAgain == "Y");
+            Console.WriteLine("Thank you for rolling with us!");
+        } 
+    }
+}
+
